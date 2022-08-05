b291b895fd6b10dd20156bc77575b2876b727420
PepeKwapien <pppkwapien@gmail.com>
2022-08-05 23:59:24 +0200

    Prepared context for entity framework

---
+++ FileApi/Data/FileContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FileApi.Data
+{
+    public class FileContext : DbContext
+    {
+        public FileContext(DbContextOptions options) : base(options) { }
+
+        DbSet<UploadedFile> uploadedFiles { get; set; }
+    }
+}
