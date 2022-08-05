7a61bda13167a15002e6a9aeed952278ff78655f
PepeKwapien <pppkwapien@gmail.com>
2022-08-05 13:32:18 +0200

    Created raw form view

--- FileFront/src/app/app.module.ts
+++ FileFront/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UploadFormComponent } from './components/upload-form/upload-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UploadFormComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, UploadFormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- FileFront/src/app/components/upload-form/upload-form.component.ts
+++ FileFront/src/app/components/upload-form/upload-form.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload-form',
   templateUrl: './upload-form.component.html',
-  styleUrls: ['./upload-form.component.css']
+  styleUrls: ['./upload-form.component.css'],
 })
 export class UploadFormComponent implements OnInit {
+  filename: string;
+  private defaultFilename = 'No chosen file';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.filename = this.defaultFilename;
   }
 
+  ngOnInit(): void {}
+
+  onSelect() {
+    this.filename = 'test.txt';
+  }
+
+  onSend() {
+    if (this.defaultFilename === this.filename) {
+      alert('Choose a file');
+      return;
+    }
+  }
 }
