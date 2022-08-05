406f5aba85099718aa184dd3c4d4b7e255f46002
PepeKwapien <pppkwapien@gmail.com>
2022-08-05 13:57:52 +0200

    Created list view

--- FileFront/src/app/app.module.ts
+++ FileFront/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UploadFormComponent } from './components/upload-form/upload-form.component';
 import { FileListComponent } from './components/file-list/file-list.component';
+import { FileListItemComponent } from './components/file-list-item/file-list-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UploadFormComponent, FileListComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UploadFormComponent,
+    FileListComponent,
+    FileListItemComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- FileFront/src/app/components/file-list/file-list.component.ts
+++ FileFront/src/app/components/file-list/file-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadedFile } from 'src/interfaces/uploadedFile';
 
 @Component({
   selector: 'app-file-list',
   templateUrl: './file-list.component.html',
-  styleUrls: ['./file-list.component.css']
+  styleUrls: ['./file-list.component.css'],
 })
 export class FileListComponent implements OnInit {
+  files: UploadedFile[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
