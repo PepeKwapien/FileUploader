1894be919306357b8f60d3f30cd72a2cdda92cb5
PepeKwapien <pppkwapien@gmail.com>
2022-08-05 12:53:09 +0200

    Started workin on a header

--- FileFront/src/app/app.module.ts
+++ FileFront/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UploadFormComponent } from './components/upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule
