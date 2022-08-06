2ecab6b9afd7c88bd9042750226f04acd54ecc4e
PepeKwapien <pppkwapien@gmail.com>
2022-08-06 10:09:32 +0200

    Added success toasts on actions

--- FileFront/src/app/components/file-list/file-list.component.ts
+++ FileFront/src/app/components/file-list/file-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UploadedFile } from 'src/interfaces/uploadedFile';
 import { HttpRequestService } from 'src/app/services/http-request.service';
 import { Subscription } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-file-list',
@@ -13,10 +14,13 @@ export class FileListComponent implements OnInit {
   private fileAddedSubscription: Subscription;
   private refreshRequested: Subscription;
 
-  constructor(private httpRequestService: HttpRequestService) {
+  constructor(
+    private httpRequestService: HttpRequestService,
+    private toastr: ToastrService
+  ) {
     this.fileAddedSubscription = this.httpRequestService
       .fileAddedSubscription()
-      .subscribe((msg) => this.getFiles());
+      .subscribe((createdFile) => this.files.push(createdFile));
     this.refreshRequested = this.httpRequestService
       .refreshSubscription()
       .subscribe((a) => this.getFiles());
@@ -29,7 +33,12 @@ export class FileListComponent implements OnInit {
   getFiles() {
     this.httpRequestService
       .getFiles()
-      .subscribe((fetchedFiles) => (this.files = fetchedFiles));
+      .subscribe(
+        (fetchedFiles) =>
+          (this.files = fetchedFiles) &&
+          fetchedFiles.length > 0 &&
+          this.toastr.success('Got all files')
+      );
   }
 
   deleteFile(file: UploadedFile) {
@@ -37,7 +46,8 @@ export class FileListComponent implements OnInit {
       .deleteFile(file)
       .subscribe(
         (fileRemoved) =>
-          (this.files = this.files.filter((f) => f.id !== file.id))
+          (this.files = this.files.filter((f) => f.id !== file.id)) &&
+          this.toastr.success(`Removed file ${file.filename}`)
       );
   }
 }
